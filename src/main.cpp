ef4c980b8a36cb98adacdc8969b27d4fb4827224
Kirill <kirill2517nv@gmail.com>
2023-02-19 11:21:50 +0700

    Added colorful triangle

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,38 @@
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
 
+GLfloat points[] =
+{
+     0.0f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
+
+GLfloat colors[] =
+{
+     1.0f,  0.0f, 0.0f,
+     0.0f,  1.0f, 0.0f,
+     0.0f,  0.0f, 1.0f
+};
+//vec4 position - (x,y,z,perspective)
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main() {"
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_position, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main() {"
+"   frag_color = vec4(color, 1.0);"
+"}";
+
 void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
 {
     g_windowSizeX = width;
@@ -60,13 +92,60 @@ int main(void)
     std::cout << "OpenGL version : " << glGetString(GL_VERSION) << std::endl;
 	
 	
-	glClearColor(1, 1, 0, 1);
+	glClearColor(0, 1, 1, 1); 
+    // create vertex shader
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER); // shader identificator
+    glShaderSource(vs, 1, &vertex_shader, nullptr); // code for shader
+    glCompileShader(vs); // compile code
+
+    // create fragment shader
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, nullptr);
+    glCompileShader(fs);
+     
+    //generate shader program for linking vs with fs
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vs);
+    glAttachShader(shader_program, fs);
+    glLinkProgram(shader_program); 
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    //vbo - vertex buffer object
+    GLuint points_vbo = 0;
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW); // переместили из оперативной памаяти в память видеокарты
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW); 
+
+    // vao - vertex array object
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3); // рисуем вершины начиная с нулевого; всего рисовать три вершины
 
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
